35fc70c6447a72fbd206ca25579e6f9b5bed1a92
Cthupby <Cthupby@gmail.com>
2022-03-20 20:54:20 +0300

    Add 1-1 tasks

---
+++ SQL-trainer/1-1-insert-into-book-1.sql
@@ -0,0 +1,9 @@
+/*
+Занесите новую строку в таблицу book (текстовые значения (тип VARCHAR) 
+*/
+
+INSERT INTO book(title, author, price, amount)
+  VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3);
+
+SELECT * FROM book;
+
