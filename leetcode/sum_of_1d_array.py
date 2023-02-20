5ed2a1e7b26d4b7e64b33704f65895a745754fa6
Cthupby <Cthupby@gmail.com>
2023-02-20 21:06:12 +0300

    add sum of 1d array

---
+++ leetcode/sum_of_1d_array.py
@@ -0,0 +1,33 @@
+"""
+1480. Running Sum of 1d Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+Example:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+"""
+class Solution:
+    def runningSum(self, nums: list[int]) -> list[int]:
+        i = 0
+        result = []
+        for v in nums:
+            i += v
+            result.append(i)
+        return result
+
+
+def test_solution(nums, result):
+    solution = Solution.runningSum(Solution, nums)
+    assert solution == result, \
+        "wrong answer {}, should be {}".format(solution, result)
+    return "assepted"
+
+
+print("Test #1: " + test_solution([1,2,3,4], [1,3,6,10]))
+print("Test #2: " + test_solution([1,1,1,1,1], [1,2,3,4,5]))
+print("Test #3: " + test_solution([3,1,2,10,1], [3,4,6,16,17]))
